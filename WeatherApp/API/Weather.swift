179adc59441a3614a2968d94c944bf429662eb57
Magnus Holm <mail@magnusholm.com>
2018-02-10 15:17:46 +0000

    Add Weather API file with all response objects as value types

---
+++ WeatherApp/API/Weather.swift
@@ -0,0 +1,45 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by Magnus Holm on 10/02/2018.
+//  Copyright © 2018 Magnus Holm. All rights reserved.
+//
+
+import Foundation
+
+class Weather: Codable {
+    
+    let forecast: Forecast
+    
+}
+
+struct Forecast: Codable {
+    let forecastText: ForecastText
+    
+    private enum CodingKeys: String, CodingKey {
+        case forecastText = "txt_forecast"
+    }
+}
+
+struct ForecastText: Codable {
+    let date: String
+    let forecastDays: [ForecastDay]
+    
+    private enum CodingKeys: String, CodingKey {
+        case date 
+        case forecastDays = "forecastday"
+    }
+}
+
+struct ForecastDay: Codable {
+    let iconUrl: String
+    let day: String
+    let description: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case iconUrl = "icon_url"
+        case day = "title"
+        case decription = "fcttext"
+    }
+}
